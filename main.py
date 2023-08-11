1c6b210dc074aa7bd3ad9545f3de174bd9ab6fd8
Douhwe <andy15001z@gmail.com>
2023-08-11 07:32:53 -0400

    fixed delete function

--- main.py
+++ main.py
@@ -1,37 +1,106 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #Unique Identifier for each stored video
+    name = db.Column(db.String(100), nullable=False) #Max 100 Characters, but title cannot be Null
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes}"
+
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the Video is required", required=True) #Argument, Type, and Help Value sent if argument wasn't found
-video_put_args.add_argument("views", type=int, help="Views of the vided is required", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes on the vdeo", required=True)
-videos = {}
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-def abortIfVideoNotFound(video_id):
-    if video_id not in videos:
-        abort(404, message="Video ID is not valid")
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Video name required")
+video_update_args.add_argument("views", type=int, help="Video Views required")
+video_update_args.add_argument("likes", type=int, help="Likes on the video required")
 
-def abortIfVideoExists(video_id):
-    if video_id in videos:
-        abort(409, message="Video with same ID already exists")
+
+#videos = {}
+
+#def abortIfVideoNotFound(video_id):
+#    if video_id not in videos:
+#        abort(404, message="Video ID is not valid")
+
+#def abortIfVideoExists(video_id):
+#    if video_id in videos:
+#        abort(409, message="Video with same ID already exists")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+
+}
+
+class VideoList(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        results = VideoModel.query.all()
+        return results
+api.add_resource(VideoList, "/video/")
 
 class Video(Resource):
+    @marshal_with(resource_fields)  #Takes fields and serializes it into a JSON format
     def get(self, video_id):
-        abortIfVideoNotFound(video_id)
-        return videos[video_id]
+        if video_id is None:
+            results = VideoModel.query.all()
+            return results
+        result = VideoModel.query.filter_by(id=video_id).first()  #Result is an instance of the VideoModel class
+        if not result:
+            abort(404, message="Video with that ID could not be found")
+        return result
     
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abortIfVideoExists(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201 #Code 201 = Created
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="This Video ID has already been used")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video) #Add
+        db.session.commit()  #Make permanent
+        return video, 201 #Code 201 = Created
+    
+    #Patching (Update)
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist")
+        if args['name'] is not None:
+            result.name = args['name']
+        if args['views'] is not None:
+            result.views = args['views']
+        if args['likes'] is not None:
+            result.likes = args['likes']
+
+
+        db.session.commit()
+        
+        return result
     
     def delete(self, video_id):
-        abortIfVideoNotFound(video_id)
-        del videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Cannot delete video, because the videoID does not exist")
+        db.session.delete(result)
+        db.session.commit()
         return '', 204
     
 #class HelloWorld(Resource): #Class inherits from resource
--- test.py
+++ test.py
@@ -1,19 +1,68 @@
-#import pip._vendor.requests
 import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 130, "name": "Brawl Stars GIVEAWAY", "views": 4500000},
-        {"likes": 500, "name": "How to build your own keyboard", "views": 1500000000},
-        {"likes": 1000, "name": "How to build your own PC", "views": 950000000}]
+#data = [{"likes": 130, "name": "Brawl Stars GIVEAWAY", "views": 4500000},
+#        {"likes": 500, "name": "How to build your own keyboard", "views": 1500000000},
+#        {"likes": 1000, "name": "How to build your own PC", "views": 950000000}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
-input()
-response = requests.delete(BASE + "video/0")
-print(response.json())
-input()
-response = requests.get(BASE + "video/2")
+#for i in range(len(data)):
+#    response = requests.put(BASE + "video/" + str(i), data[i])
+#    print(response.json())
+
+#input()
+
+def displayVideos(videos):
+    for video in videos:
+        print("Video ID:", video["id"])
+        print("Name:", video["name"])
+        print("Views:", video["views"])
+        print("Likes:", video["likes"])
+
+response = requests.put(BASE + "video/2", {"name": "How to build your own PC", "views": 100, "likes": 50})
+
 print(response.json())
 
+response = requests.put(BASE + "video/3", {"name": "How to build your ROBLOX game", "views": 100000, "likes": 550})
+print(response.json())
+
+response = requests.patch(BASE + "video/2", {"name": "[UPDATED] How to build your own PC", "views": 750, "likes": 150})
+print(response.json())
+
+response = requests.get(BASE + "video/")
+if response.status_code != 200:
+    print(f"Error: received status code {response.status_code}")
+    exit()
+print("Raw Response:", response.text)
+
+try:
+    data = response.json()
+except ValueError:
+    print("Error")
+    exit()
+
+
+
+
+
+
+# videos = data if isinstance(data, list) else []
+# displayVideos(videos)
+
+videoIDToDelete = 2
+
+response = requests.delete(BASE + "video/" + str(videoIDToDelete))
+
+if response.status_code == 204:
+    print(f"Video with ID {videoIDToDelete} has been successfully deleted")
+else:
+    print(f"Error: Received Status Code {response.status_code}: {response.text}")
+
+
+response = requests.get(BASE + "video/")
+
+if response.status_code != 200:
+    print(f"Get Error: Received status code {response.status_code}")
+    exit()
+videos = response.json()
+displayVideos(videos)
